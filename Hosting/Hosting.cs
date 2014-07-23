1759d4f1bd70be9a3406a58ae4c4ed767ba8e854
zZED <xingxing0468@hotmail.com>
2014-07-23 11:24:05 +0800

    Import
    1st time import all the files in the project

---
+++ Hosting/Hosting.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using System.Xml.XmlConfiguration;
+
+using Artech.WcfServices.Contracts;
+using Artech.WcfServices.Services;
+namespace Hosting
+{
+    class Hosting
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost host = new ServiceHost(typeof(CalServices));
+
+            NetTcpBinding binding = new NetTcpBinding();
+            binding.ReaderQuotas
+        }
+    }
+}
