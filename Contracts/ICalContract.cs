897b2a48e0ade009956f90288ce73285d2c39cac
zZED <xingxing0468@hotmail.com>
2014-07-23 11:31:41 +0800

    Revert "Revert "Import""
    This reverts commit 92e8ee00d9c5c99345c7d36466e9b6f6962a1ebb.

---
+++ Contracts/ICalContract.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.ServiceModel;
+
+namespace Artech.WcfServices.Contracts
+{
+    [ServiceContract]
+    public interface ICal
+    {
+        [OperationContract]
+        double Add(double x, double y);
+
+        [OperationContract]
+        double Subtract(double x, double y);
+
+        [OperationContract]
+        double Multiply(double x, double y);
+
+        [OperationContract]
+        double Divide(double x, double y);
+    };
+}
+
+
